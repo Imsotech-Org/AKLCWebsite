7f59b3f34b70dd29dfb3a3fa56946ae92d612dec
layuntaFelipe <layunta.felipe@gmail.com>
2022-11-24 00:22:25 -0700

    testimonial-proto-component-done

--- frontend/src/components/TestimonialsItem.jsx
+++ frontend/src/components/TestimonialsItem.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {FaStar} from 'react-icons/fa';
 
 const TestimonialsItem = () => {
   return (
-    <div>TestimonialsItem</div>
+    <div className='testimonialsItemContainer'>
+      <div className='testimonialsItemHeader'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta neque tempore laborum vitae amet sunt expedita accusantium asperiores repellat ducimus.</p>
+        <small>John Doe, Since 2019</small>
+      </div>
+      <div className='testimonialsItemStars'>
+        <FaStar/>
+        <FaStar/>
+        <FaStar/>
+        <FaStar/>
+        <FaStar/>
+      </div>
+    </div>
   )
 }
 
