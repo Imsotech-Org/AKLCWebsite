c42be7653663b1cdb60d0cb68ea9d005edbbf9c9
layuntaFelipe <layunta.felipe@gmail.com>
2022-12-01 00:51:09 -0700

    first-privacyPolicy-page-done

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Footer from '../components/Footer';
 
 const PrivacyPolicy = () => {
   return (
-    <div>PrivacyPolicy</div>
+    <div style={{ paddingTop: '5.5rem' }}>
+      <div className="privacyPolicyContainer" style={{margin: '5rem 15rem'}}>
+        <h2 style={{color: '#879635', fontSize: '2.5rem', marginBottom: '5rem'}}>Privacy, Data & Terms of Use for Andrew Kolasko Life Center</h2>
+        <p style={{color: '#363d10', fontSize: '1.3rem', marginBottom: '4rem'}}>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita a iure id ex sit illo debitis, praesentium quaerat at deserunt quas, porro quis nostrum iusto quibusdam eos voluptatum amet consectetur ipsa nobis excepturi? Veniam incidunt, repellendus illo iste repellat, consequuntur modi nostrum provident iusto ab odit ullam! Adipisci voluptas doloremque sed dolore neque voluptatibus quis aliquam veniam sequi ad numquam dicta pariatur iure illo obcaecati consequuntur deserunt, fuga rem nulla corporis necessitatibus facilis molestias cupiditate qui! Officia ipsam rem iure.</p>
+        <p style={{color: '#363d10', fontSize: '1.3rem', marginBottom: '4rem'}}>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita a iure id ex sit illo debitis, praesentium quaerat at deserunt quas, porro quis nostrum iusto quibusdam eos voluptatum amet consectetur ipsa nobis excepturi? Veniam incidunt, repellendus illo iste repellat, consequuntur modi nostrum provident iusto ab odit ullam! Adipisci voluptas doloremque sed dolore neque voluptatibus quis aliquam veniam sequi ad numquam dicta pariatur iure illo obcaecati consequuntur deserunt, fuga rem nulla corporis necessitatibus facilis molestias cupiditate qui! Officia ipsam rem iure.</p>
+        <p style={{color: '#363d10', fontSize: '1.3rem', marginBottom: '4rem'}}>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita a iure id ex sit illo debitis, praesentium quaerat at deserunt quas, porro quis nostrum iusto quibusdam eos voluptatum amet consectetur ipsa nobis excepturi? Veniam incidunt, repellendus illo iste repellat, consequuntur modi nostrum provident iusto ab odit ullam! Adipisci voluptas doloremque sed dolore neque voluptatibus quis aliquam veniam sequi ad numquam dicta pariatur iure illo obcaecati consequuntur deserunt, fuga rem nulla corporis necessitatibus facilis molestias cupiditate qui! Officia ipsam rem iure.</p>
+        <p style={{color: '#363d10', fontSize: '1.3rem', marginBottom: '4rem'}}>&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita a iure id ex sit illo debitis, praesentium quaerat at deserunt quas, porro quis nostrum iusto quibusdam eos voluptatum amet consectetur ipsa nobis excepturi? Veniam incidunt, repellendus illo iste repellat, consequuntur modi nostrum provident iusto ab odit ullam! Adipisci voluptas doloremque sed dolore neque voluptatibus quis aliquam veniam sequi ad numquam dicta pariatur iure illo obcaecati consequuntur deserunt, fuga rem nulla corporis necessitatibus facilis molestias cupiditate qui! Officia ipsam rem iure.</p>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
