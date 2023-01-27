908b861d703e7845de760428b4902e086866c8b7
layuntaFelipe <layunta.felipe@gmail.com>
2023-01-27 16:59:31 -0700

    ready-to-deploy-final-4

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,10 +2,13 @@ import { Navigate, Outlet } from "react-router-dom";
 import {useAuthStatus} from '../hooks/useAuthStatus';
 
 const PrivateRoute = () => {
+ const {loggedIn, loading} = useAuthStatus();
 
-    const {loggedIn, checking} = useAuthStatus();
+ if(loading) {
+  return <h1>Loading...</h1>
+ }
 
-    return loggedIn ? <Outlet /> : <Navigate to='/credentials' />
+  return loggedIn ? <Outlet /> : <Navigate to='/credentials' />
 }
 
 export default PrivateRoute
