c84aeb8f38585abf706b1bd3050281cf4a2bceaa
layuntaFelipe <layunta.felipe@gmail.com>
2023-01-29 18:33:41 -0700

    api-youtube-fixed

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -7,73 +7,50 @@ import {getYoutubeVideos, createYoutubeVideo} from '../features/youtubeVideos/yo
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import { set } from 'mongoose';
 
 const Blog = () => {
 
   const {youtubeVideos, isError, isSuccess, message} = useSelector((state) => state.youtubeVideo);
+  const [calledOnce, setCalledOnce] = useState(false);
 
   const dispatch = useDispatch();
 
   const [ytVideos, setYtVideos] = useState([
-    {
-      imageName: 'https://i.ytimg.com/vi/bBmNs8b8IIQ/hqdefault.jpg',
-      title: 'Aged 45 to 65? Bring your A+ game!!',
-      description: "In this video, I'll show you info about Andrew Kolasko Life Center, and a description of who Andrew Kolasko is. Enjoy! ABOUT MY ...",
-      youtubevideoId: 'bBmNs8b8IIQ'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/3SjM13JDdmM/hqdefault.jpg',
-      title: 'Healthier and Fitter in your 40s, 50s, 60s+',
-      description: "If you're male ages 45-65 and are trying to figure out how to focus on your health, and fitness and promote longevity living, this is ...",
-      youtubevideoId: '3SjM13JDdmM'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/bBmNs8b8IIQ/hqdefault.jpg',
-      title: 'Aged 45 to 65? Bring your A+ game!!',
-      description: "In this video, I'll show you info about Andrew Kolasko Life Center, and a description of who Andrew Kolasko is. Enjoy! ABOUT MY ...",
-      youtubevideoId: 'bBmNs8b8IIQ'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/lte-1_l2Lgc/hqdefault.jpg',
-      title: 'The Plus+ podcast #1 Is it better to look good or live a happy, healthy, longer life?',
-      description: "This clip is from podcast # ‒ Is it better to look good or live a happy, healthy longer life? |Andrew Kolasko| Advanced Human ...",
-      youtubevideoId: 'lte-1_l2Lgc'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/CWJb8jCRDWw/hqdefault.jpg',
-      title: 'The Plus+ podcast #2 The Power of Outsourcing',
-      description: "This clip is from podcast #2 ‒ The Power Of Outsourcing |Andrew Kolasko | Advanced Human Performance Specialist Sign up to ...",
-      youtubevideoId: 'CWJb8jCRDWw'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/uIQQEhJ7FR4/hqdefault.jpg',
-      title: 'The Plus+ podcst #3  How to increase your Longevity (Part1)',
-      description: "This clip is from podcast # ‒How to increase your Longevity (Part1) |Andrew Kolasko| Advanced Human Performance Specialist ...",
-      youtubevideoId: 'uIQQEhJ7FR4'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/PnG7TsFvjEs/hqdefault.jpg',
-      title: 'The Plus+ podcast #4 The Why',
-      description: "This clip is from podcast #4 ‒ The why |Andrew Kolasko | Advanced Human Performance Specialist #longevity #fitness #health ...",
-      youtubevideoId: 'PnG7TsFvjEs'
-    },
-    {
-      imageName: 'https://i.ytimg.com/vi/H4Eupz5tKok/hqdefault.jpg',
-      title: 'The Plus+ podcast #5 What Really Matters',
-      description: "This clip is from podcast #5 ‒ What Really Matters |Andrew Kolasko| Advanced Human Performance Specialist Sign up to receive ...",
-      youtubevideoId: 'H4Eupz5tKok'
-    },
+    {}
   ]);
 
   useEffect(() => {
 
-    dispatch(getYoutubeVideos());
-
-    if(youtubeVideos.length === 0){
-      console.log('EMPTY');
-      ytVideos.forEach(element => {
-        console.log('Criando youtube')
-        dispatch(createYoutubeVideo(element));
+    if(!calledOnce){
+      dispatch(getYoutubeVideos());
+      let data;
+      axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCXFClXHhtv4hog6VNrvCZZHeJAuntNcvw&channelId=UCScUFoeTbUXWU9FTsKJPCOA&part=snippet,id&order=date&maxResults=20').then(
+        (response) => {
+          data = response;
+          data.data.items.map((item, index) => {
+            if(item.id.kind === "youtube#video"){
+              setYtVideos(oldArray => [...oldArray, {
+                imageName: item.snippet.thumbnails.high.url,
+                title: item.snippet.title,
+                description: item.snippet.description,
+                youtubevideoId: item.id.videoId
+              }]);
+              if(item.snippet.title !== youtubeVideos.title){
+                dispatch(createYoutubeVideo(
+                  {
+                    imageName: item.snippet.thumbnails.high.url,
+                    title: item.snippet.title,
+                    description: item.snippet.description,
+                    youtubevideoId: item.id.videoId
+                  }
+                ));
+              }
+            }
+          })
+        }
+        ).catch(function (error) {
+        console.log(error);
       });
     }
 
